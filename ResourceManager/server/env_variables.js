da95d41a571de63442db84e84f42205344624db5
Allan Kiplagat <allan910@gmail.com>
2016-01-25 19:28:38 -0500

    Added 'email' and 'check' packages.

---
+++ ResourceManager/server/env_variables.js
@@ -0,0 +1,3 @@
+Meteor.startup(function () {
+    process.env.MAIL_URL = 'smtp://postmaster@sandboxcec5d7b99916449899850efb807be6ff.mailgun.org:4b26fe1188060b76a542477bb5812db2@smtp.mailgun.org:587';
+});
