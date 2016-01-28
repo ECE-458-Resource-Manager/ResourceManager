cf158eb10da36a4b820e0df2d804b492d491e32b
Emmanuel Shiferaw <eshiferax@gmail.com>
2016-01-28 14:16:45 -0500

    Added collection2 package for schema validation on insert/update

--- ResourceManager/lib/collections/emails.js
+++ ResourceManager/lib/collections/emails.js
@@ -5,3 +5,19 @@ Emails = new Mongo.Collection('emails');
  * sent_at: datetime
  * contents: String
  */
+EmailSchema = new SimpleSchema({
+    sent_to: {
+        type: String,
+        label: "Sent To"
+    },
+    sent_at: {
+        type: Date,
+        label: "Sent At"
+    },
+    contents: {
+        type: String,
+        label: "Contents"
+    }
+});
+
+Emails.attachSchema(EmailSchema);
--- ResourceManager/lib/collections/reservations.js
+++ ResourceManager/lib/collections/reservations.js
@@ -9,3 +9,32 @@ Reservations = new Mongo.Collection('reservations');
  * end_date (datetime)
  * cancelled - Bool
  */
+
+ReservationSchema = new SimpleSchema({
+    owner_id: {
+        type: [Number],
+        label: "Owner ID"
+    },
+    attending_user_id: {
+        type: [Number],
+        label: "Attending User ID"
+    },
+    resources: {
+        type: [Object],
+        label: "Resources Reserved" 
+    },
+    start_date: {
+        type: Date,
+        label: "Start date/time"
+    },
+    end_date: {
+        type: Date,
+        label: "End date/time"
+    },
+    cancelled: {
+        type: Boolean,
+        label: "Cancelled"
+    }
+});
+
+Reservations.attachSchema(ReservationSchema);
