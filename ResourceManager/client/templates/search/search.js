4317f6eb53a0c53b8a8d9b55cc0b8c9c3974474a
Emmanuel Shiferaw <eshiferax@gmail.com>
2016-02-10 09:26:11 -0500

    timespan imp. attempt

--- ResourceManager/client/templates/search/search.js
+++ ResourceManager/client/templates/search/search.js
@@ -1,3 +1,9 @@
+startTimeKey = 'startTimeKey';
+endTimeKey = 'endTimeKey';
+startDateKey = 'startDateKey'
+endDateKey = 'endDateKey';
+
+
 ResourcesFilter = new FilterCollections(Resources, {
     template: 'search',
     name: 'filter-collections-resources', // should match publish name
@@ -19,6 +25,18 @@ Template.search.events({
         // Not using search methods (buggy)
         Session.set(searchEntryKey, e.target.value);
         runFilters();
+    },
+    'input #start_date': function(e) {
+        Session.set(startDateKey, e.target.value);
+    },
+    'input #end_date': function(e) {
+        Session.set(endDateKey, e.target.value);
+    },
+    'input #start_time': function(e) {
+        Session.set(startTimeKey, e.target.value);
+    },
+    'input #end_time': function(e) {
+        Session.set(endTimeKey, e.target.value);
     }
 });
 
@@ -32,12 +50,24 @@ Template.search.rendered = function () {
 
 Template.search.helpers({
     isAvailable: function(resource) {
-        Meteor.call('getReservationStream', resource, null, null, true, function(error, result){
-            errorHandle(error);
-            var params = result;
-           });
+        var start = Session.get(startDateKey) + Session.get(startTimeKey);
+        var end = Session.get(endDateKey) + Session.get(endTimeKey);
+        //Is this a legitimate way to build the dates we're searching on?
         
+        var resObjects = []
+        Meteor.call('getReservationStream', resource, start, end, false, function(error, result){
+              resObjects = result;
+           }
+        );
+        for(var i = 0; i < resObjects.length; i++) {
+            //here basically check if resObjects[i]'s start or end is between that queried
+            if(resObjects[i].start) {
+                return false;
+            }
+        }
 
         return true;
     }
 });
+
+
