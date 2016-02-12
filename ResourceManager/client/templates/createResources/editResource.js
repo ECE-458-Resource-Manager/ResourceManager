4723b43ff62be7ff878629ccfea5ce52dfeb057c
Davis Gossage <dcg13@duke.edu>
2016-02-12 14:41:22 -0500

    Add delete button to resource, with appropriate warning for existing reservations

--- ResourceManager/client/templates/createResources/editResource.js
+++ ResourceManager/client/templates/createResources/editResource.js
@@ -1,10 +1,10 @@
-var prevId = "";
+var resource = null;
 
 Template.editResource.rendered = function() {
 	Session.set(selectedTagsKey, []); // clear tags
 	$("#resourceNameInput")[0].value = this.data.name;
 	$("#resourceDescriptionInput")[0].value = this.data.description;
-	prevId = this.data._id;
+	resource = this.data;
 	var selectedTags = this.data.tags;
     if (!selectedTags) selectedTags = [];
     Session.set(selectedTagsKey, selectedTags);
@@ -20,9 +20,9 @@ Template.editResource.events({
 		if (!selectedTags) selectedTags = [];
 
 		Resources.update(
-			{_id: prevId},
+			{_id: resource._id},
 			{ $set:{
-				_id: prevId,
+				_id: resource._id,
 				name: resourceName,
 				description: resourceDescription,
 				tags: selectedTags,
@@ -38,6 +38,31 @@ Template.editResource.events({
 		Router.go('mainPage');
 
 		// Prevent default form submit
+		return false;
+	},
+
+	"click .delete-button": function (event) {
+		event.preventDefault();
+		Meteor.call('queryReservations', resource, new Date(), null, null, function(error, result){
+			if (result.length > 0){
+				MaterializeModal.message({
+			    title: 'Confirm',
+			    submitLabel: 'Yes',
+			    closeLabel: 'Cancel',
+			    message: "This resource has reservations which haven't occurred yet.  Are you sure you want to delete it?",
+			    callback: function(error, response){
+			      if (response.submit){
+			        Resources.remove({_id: resource._id});
+			        Router.go('mainPage');
+			      }
+			    }
+			  });
+			}
+			else{
+				Resources.remove({_id: resource._id});
+			  Router.go('mainPage');
+			}
+		});
 		return false;
 	}
 });
