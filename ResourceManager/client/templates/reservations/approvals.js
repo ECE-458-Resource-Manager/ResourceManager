0c09fc0b642e04ad68f87a67faf375418ec7d130
Sam Ginsburg <sbg11@duke.edu>
2016-03-28 15:23:58 -0400

    added correct filtering to approvals page and fixed bug when viewing reservation details

--- ResourceManager/client/templates/reservations/approvals.js
+++ ResourceManager/client/templates/reservations/approvals.js
@@ -1,6 +1,19 @@
 Template.approvals.helpers({
     reservationsToApprove: function() {
     	console.log(Meteor.user())
-        return Reservations.find({});
+
+        var userID = Meteor.user()._id;
+
+        if(Roles.userIsInRole(userID, ["admin", "manage-reservations"])){
+        	return Reservations.find({incomplete: true});
+        }
+
+    	var allPermissions = Roles.getRolesForUser(userID);
+    	var params = {
+        	approvers: {$in: allPermissions},
+        	incomplete: true
+    	}
+   		
+    	return Reservations.find(params);
     }
 });
